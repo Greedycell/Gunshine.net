4d250cc73455d1162c55a559ac7e773d3439e3ce
Astral <132731805+astralsc@users.noreply.github.com>
2026-02-03 22:15:48 +0800

    Update CreatePlayerMessage.js

--- Protocol/Messages/Client/CreatePlayerMessage.js
+++ Protocol/Messages/Client/CreatePlayerMessage.js
@@ -3,29 +3,49 @@ const CreatePlayerOkMessage = require('../Server/CreatePlayerOkMessage')
 const CreatePlayerFailedMessage = require('../Server/CreatePlayerFailedMessage')
 
 class CreatePlayerMessage extends PiranhaMessage {
-  constructor (bytes, client) {
+  constructor(bytes, client) {
     super(bytes)
     this.client = client
     this.id = 10200
     this.version = 1
   }
 
-  async decode () {
+  async decode() {
     this.data = {}
 
-    this.data.Name = this.readString()
-    this.data.CharacterData = this.readString()
+    this.data.name = this.readString()
+    this.data.characterDataId = this.readInt()
+    this.data.propsCount = this.readInt()
+    this.data.props = []
+    for (let i = 0; i < this.data.propsCount; i++) {
+      this.data.props.push(this.readInt())
+    }
 
-    console.log(this.data)
+    //console.log(this.data)
   }
 
-  async process () {
-    this.writeString('Astral') // Name
-    this.writeInt(0) // CharacterData
-    this.writeInt(0) // Unknown
+  async process() {
+    const playerData = {
+      idHigh: 0,
+      idLow: this.client.accountId || 1,
+      name: this.data.name,
+      characterDataId: this.data.characterDataId,
+      level: 1,
+      maxHealth: 100,
+      maxEnergy: 100,
+      missionBitListSize: 8,
+      visitedLevelsBitListSize: 8,
+      secondBitListSize: 8,
+      homeLevelId: 0,
+      props: this.data.props
+    }
 
-    //await new CreatePlayerOkMessage(this.client).send()
-    await new CreatePlayerFailedMessage(this.client).send()
+    this.client.player = playerData
+
+    this.client.log('Creating player:', playerData.name, 'CharacterData:', playerData.characterDataId)
+
+    await new CreatePlayerOkMessage(this.client, playerData).send()
+    //await new CreatePlayerFailedMessage(this.client).send()
   }
 }
 
